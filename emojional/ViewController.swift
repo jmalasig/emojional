39ba1bb0b02a436b4cefe8f8cc20038f4bcbda88
jmalasig <joanamalasig@gmail.com>
2019-05-27 18:29:53 -0400

    Complete Iteration 2

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emojis = ["🐶": "-doggo", "🐱":"-meowman"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func showMessage(sender: UIButton){
-        let alertController = UIAlertController(title: "Hi!", message: "Bye!", preferredStyle: UIAlertController.Style.alert)
+        let selectedEmotion = sender.titleLabel?.text
+        let alertController = UIAlertController(title: "Hi!", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil
         )
